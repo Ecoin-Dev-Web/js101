20c572f009540565d268af4427f3d1442c57b1c3
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-05-06 14:05:54 +0100

    callvack fn

--- js/fun.js
+++ js/fun.js
@@ -30,7 +30,7 @@ var welcom =  function (a,b, op){
 }
 
 //console.log(typeof welcom)
-console.log(welcom(25,6,"+"))
+/*console.log(welcom(25,6,"+"))
 console.log(welcom(25,6,"-"))
 
 var t = function (a) {
@@ -42,3 +42,27 @@ console.log(t(5));
 (function (a) {
 	console.log(a*100)
 })(3)
+
+//b callback
+function ecoin2(a,b){
+    console.log( b())
+}
+var c = function () {
+	return 100
+}
+ecoin2(14,c())
+*/
+function test(t,fn){
+	return fn(t)
+}
+
+var tab = [5,3,6,12]
+
+
+test(tab,function (tab) {
+	var tab2 = new Array()
+	for(var i = 0 ; i<tab.length;i++){
+         tab2[i] = tab[i]*2
+	}
+	console.log(tab2)
+})
