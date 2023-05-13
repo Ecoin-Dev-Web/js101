3c518efa6fcdda714485b8aacb9fd30e682fe0e7
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-05-13 10:08:03 +0100

    update

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,19 @@
-console.log('just a test')
+var bx = document.getElementById("box")
+
+console.log(bx)
+
+var mycl = document.getElementsByClassName("cl")
+console.log(mycl[0])
+
+var mylist = document.getElementsByTagName("li")
+console.log(mylist)
+
+
+/*bx.innerText = "Welcom to Ecoin"
+bx.innerHTML = "<i>Welcom to WEB</i>"*/
+
+var mytitle = document.getElementById("t")
+console.log(mytitle)
+var parent  = mytitle.parentNode
+console.log(parent)
+parent.removeChild(mytitle)
