d500d03b02957d4a8da055e2420c9475ae2fb1c0
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-05-13 10:09:20 +0100

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -8,10 +8,8 @@ console.log(mycl[0])
 var mylist = document.getElementsByTagName("li")
 console.log(mylist)
 
-
 /*bx.innerText = "Welcom to Ecoin"
 bx.innerHTML = "<i>Welcom to WEB</i>"*/
-
 var mytitle = document.getElementById("t")
 console.log(mytitle)
 var parent  = mytitle.parentNode
