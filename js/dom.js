a2a185b979201149d8dc03086514b444a456c0ff
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-05-13 10:46:46 +0100

    update Attribute

--- js/dom.js
+++ js/dom.js
@@ -7,7 +7,6 @@ console.log(mycl[0])
 
 var mylist = document.getElementsByTagName("li")
 console.log(mylist)
-
 /*bx.innerText = "Welcom to Ecoin"
 bx.innerHTML = "<i>Welcom to WEB</i>"*/
 var mytitle = document.getElementById("t")
@@ -15,3 +14,18 @@ console.log(mytitle)
 var parent  = mytitle.parentNode
 console.log(parent)
 parent.removeChild(mytitle)
+
+
+var myNode = document.createElement("h4")
+
+myNode.innerText = "This My new Node"
+
+bx.appendChild(myNode)
+
+// attribute
+
+var mylink = document.getElementById("gl")
+mylink.setAttribute("ecoin", "Web")
+mylink.setAttribute("href", "https://www.google.com")
+
+mylink.removeAttribute("ecoin")
