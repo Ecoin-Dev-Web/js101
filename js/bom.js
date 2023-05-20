a71e45c0dda42f142c4b3cc91b5ab780a1f0719b
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-05-20 13:44:53 +0100

    update

--- js/bom.js
+++ js/bom.js
@@ -1,3 +1,29 @@
-setTimeout(function () {
+/*setTimeout(function () {
    location.href = 'https://www.google.com'
-},5000)
+},5000)*/
+
+var btn = document.querySelector("button")
+btn.onclick = function () {
+	var ip = document.querySelector('input[type=text]')
+	//console.log(ip.value.length)
+	var cpt = ip.value.length
+	//var sec= cpt 15
+
+var handler = 	setInterval(function () {
+  // location.href = 'https://www.google.com'
+		cpt--
+		if (cpt==0) {
+			clearInterval(handler)
+			location.href = "https://www.google.com"
+		}
+	    	 
+        document.querySelector('.counter').innerText = cpt
+},1000)
+	 
+	
+
+
+
+//
+
+}
