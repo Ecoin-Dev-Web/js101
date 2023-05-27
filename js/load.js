de6bd81433bc12058cc8809218a35e8dd9d5cf27
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-05-27 09:10:41 +0100

    update

---
+++ js/load.js
@@ -0,0 +1,13 @@
+
+window.onload = function () {
+	var bx = document.querySelector('.box')
+console.log(bx)
+
+bx.style.backgroundColor = 'blue'
+bx.style.color = '#fff'
+bx.style.padding = '7px'
+
+
+
+}
+
